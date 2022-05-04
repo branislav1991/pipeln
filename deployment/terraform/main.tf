995db5a52a8fd9fdc61c4b69626d0d3eebcf1914
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-04 23:10:42 +0200

    Mark acr username and password as sensitive

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -62,9 +62,11 @@ output "acr_login_server" {
 }
 
 output "acr_login_username" {
-  value = azurerm_container_registry.acr.admin_username
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
 }
 
 output "acr_login_password" {
-  value = azurerm_container_registry.acr.admin_password
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
