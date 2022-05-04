4aa5ef5e72804b275acb6aa29ccfb3d41dfcaca0
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-04 23:04:02 +0200

    Update workflow to push services container to ACR

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -54,4 +54,17 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
+  admin_enabled       = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_login_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_login_password" {
+  value = azurerm_container_registry.acr.admin_password
 }
