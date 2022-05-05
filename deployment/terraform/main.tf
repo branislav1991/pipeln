220fbaa439e3340c3f644b8664ee4591b6fab60b
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-05 12:40:01 +0200

    Deploy AKS cluster

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -70,3 +70,27 @@ output "acr_login_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "pipelnservicecluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "pipeln-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "ara" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
