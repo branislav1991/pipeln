910ce149047958d818b4d76df6c2afac99b4cb86
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-06 18:07:07 +0200

    Update aks subnet

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -323,13 +323,13 @@ resource "azurerm_virtual_network" "aks" {
   name                = "aks-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/8"]
 }
 resource "azurerm_subnet" "aks" {
   name                 = "aks-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.240.0.0/16"]
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
