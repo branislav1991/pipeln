69ba461d724c28701550fedd53b2a4fbe767bd2b
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-17 20:49:15 +0200

    Add verification method to create contract dialog

--- frontend/src/popups/CreateContract.js
+++ frontend/src/popups/CreateContract.js
@@ -10,6 +10,41 @@ import { BsChevronExpand, BsCircleFill, BsExclamationTriangle } from 'react-icon
  * @fileoverview Popup to create a new contract (usually shown in Contracts screen)
  */
 
+const verifications = [
+    "Automatic",
+    "Manual",
+]
+
+const VerificationListBox = () => {
+    const [selectedVerification, setSelectedVerification] = useState("Automatic");
+
+    return (
+        <Listbox value={selectedVerification} onChange={setSelectedVerification}>
+            <Listbox.Button className="relative flex items-center w-full cursor-pointer rounded-lg py-2 pl-3 pr-10 text-left text-sm border border-gray-300 text-gray-700">
+                <span className="truncate">{selectedVerification}</span>
+                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <BsChevronExpand
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                    />
+                </span>
+            </Listbox.Button>
+            <div className="relative w-full">
+                <Listbox.Options className="absolute width-full mt-1 max-h-60 w-full rounded-md bg-white py-1 text-sm border border-gray-300">
+                    {verifications.map((ver, idx) => (
+                        <Listbox.Option
+                            key={idx}
+                            value={ver}
+                            className={({ active }) => `relative items-center flex space-x-2 cursor-default select-none py-2 pl-4 pr-4 ${active ? "bg-blue-600 text-white" : "text-gray-700"}`}>
+                            <span className="grow">{ver}</span>
+                        </Listbox.Option>
+                    ))}
+                </Listbox.Options>
+            </div>
+        </Listbox>
+    );
+}
+
 const steps = [
     {
         "title": "Receiver",
@@ -17,7 +52,7 @@ const steps = [
     },
     {
         "title": "Verification",
-        "content": "Content"
+        "content": <VerificationListBox />
     },
     {
         "title": "Authentication",
@@ -37,7 +72,7 @@ const MyEndpointsListBox = ({ endpoints }) => {
     return (
         <>
             <Listbox value={selectedEndpoint} onChange={setSelectedEndpoint}>
-                <Listbox.Button className="relative flex items-center w-full cursor-pointer rounded-lg py-2 pl-3 pr-10 text-left border border-gray-300 text-gray-700">
+                <Listbox.Button className="relative flex items-center w-full cursor-pointer rounded-lg py-2 pl-3 pr-10 text-left border border-gray-300 text-gray-700 text-sm">
                     <span className={`mr-2 ${selectedEndpoint ? "" : "hidden"} ${selectedEndpoint && selectedEndpoint["active"] ? "text-green-500" : "text-red-500"}`}><BsCircleFill className="w-4 h-auto" /></span>
                     <span className="truncate">{selectedEndpoint ? selectedEndpoint["address"] : "Select Endpoint"}</span>
                     <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
@@ -48,12 +83,12 @@ const MyEndpointsListBox = ({ endpoints }) => {
                     </span>
                 </Listbox.Button>
                 <div className="relative w-full">
-                    <Listbox.Options className="absolute width-full mt-1 max-h-60 w-full rounded-md bg-white py-1 text-base border border-gray-300">
+                    <Listbox.Options className="absolute width-full mt-1 max-h-60 w-full rounded-md bg-white py-1 text-sm border border-gray-300">
                         {endpoints.map((ep) => (
                             <Listbox.Option
                                 key={ep["id"]}
                                 value={ep}
-                                className="relative items-center flex space-x-2 cursor-default select-none py-2 pl-4 pr-4 text-gray-700">
+                                className={({ active }) => `relative items-center flex space-x-2 cursor-default select-none py-2 pl-4 pr-4 ${active ? "bg-blue-600 text-white" : "text-gray-700"}`}>
                                 <span><BsCircleFill className={`w-4 h-auto ${ep["active"] ? "text-green-500" : "text-red-500"}`} /></span>
                                 <span className="grow">{ep["address"]}</span>
                             </Listbox.Option>
@@ -75,6 +110,25 @@ function CreateContract({ onClose }) {
     const fakeUserId = 1;
     const [endpointStep, setEndpointStep] = useState({ "title": endpointStepName, "content": "Loading..." });
     const [loadingEndpoints, setLoadingEndpoints] = useState(true);
+    const [searchingUsers, setSearchingUsers] = useState(false);
+
+    useEffect(() => {
+        async function getMyEndpoints() {
+            await getEndpointsForUser(fakeUserId)
+                .then((endpoints) => {
+                    setLoadingEndpoints(false);
+                    setEndpointStep({
+                        "title": endpointStepName,
+                        "content": <MyEndpointsListBox endpoints={endpoints} />
+                    });
+                })
+                .catch(error => console.error(error));
+        }
+
+        if (loadingEndpoints) {
+            getMyEndpoints();
+        }
+    }, [loadingEndpoints]);
 
     useEffect(() => {
         async function getMyEndpoints() {
