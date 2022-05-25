cead6f82648c5a157616ce8a29fb445c730c73ca
Branislav Holländer <branislav.hollander@gmail.com>
2022-05-25 23:09:10 +0200

    Update react to 18.1

--- frontend/src/dashboard-cards/Dataflow.js
+++ frontend/src/dashboard-cards/Dataflow.js
@@ -5,48 +5,6 @@
  */
 
 import { BsBarChart } from 'react-icons/bs'
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement
-);
-
-const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            display: false,
-        },
-    },
-};
-
-const labels = ['Jan 2022', 'Feb 2022', 'Mar 2022', 'Apr 2022', 'May 2022', 'Jun 2022', 'Jul 2022'];
-const data = [-101, -291, 176, 409, -172, 68, 329];
-const bgColors = ['rgba(205, 28, 28, 0.5)',
-    'rgba(205, 28, 28, 0.5)',
-    'rgba(21, 158, 61, 0.5)',
-    'rgba(21, 158, 61, 0.5)',
-    'rgba(205, 28, 28, 0.5)',
-    'rgba(21, 158, 61, 0.5)',
-    'rgba(21, 158, 61, 0.5)'];
-
-const chartData = {
-    labels,
-    datasets: [
-        {
-            data: data,
-            backgroundColor: bgColors,
-        },
-    ],
-};
 
 function Dataflow() {
     return (
@@ -57,7 +15,7 @@ function Dataflow() {
                     <h2>Data Flow</h2>
                 </div>
                 <div className="dashboard-card-content">
-                    <Bar options={options} data={chartData} />
+                    <p>Statistical data</p>
                 </div>
                 <div className="dashboard-card-footer items-center">
                     <button type="button" className="button-full">View Statistics</button>
